f6d5aa56e64e54e3e5a8d8b618b67d2f24c77114
dustinusey <dustinusey@me.com>
2022-11-07 16:33:15 -0600

    added support for dark mode w theme

--- src/components/themes/DarkMode.js
+++ src/components/themes/DarkMode.js
@@ -4,7 +4,7 @@ export default function DarkMode(props) {
   return (
     <div>
       <h3>Dark Mode</h3>
-      <input type='checkbox' className="darkMode-selector" style={{background:props.color}} onChange={() => props.toggleDarkMode()}/>
+      <input type='checkbox' className="darkMode-selector" onChange={() => props.toggleDarkMode()}/>
     </div>
   );
 }
